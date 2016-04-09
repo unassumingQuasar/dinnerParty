33b5fec3897b1d3fff2d8432b57ed73ba702db3f
CodeLizards <ebharris93@gmail.com>
2016-04-08 21:20:41 -0700

    Updated Server file for modularity with google auth

--- server/server.js
+++ server/server.js
@@ -2,61 +2,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var db = require('./config/init.js')
 var app = express();
 var port = 3000;
 
-
-   
-  passport.use(new GoogleStrategy({
-      clientID:     google.GOOGLE_CLIENT_ID,
-      clientSecret: google.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/googlecallback",
-      passReqToCallback   : true
-    },
-    function(request, accessToken, refreshToken, profile, done) {
-      console.log('GOOGLY LOGIN', profile);
-      
-      db.User.findOrCreate({where: { googleId: profile.id.toString()}, defaults: { name: profile.displayName}}).then(function (user, created) {
-        console.log("PSQL USER", user, 'created', created);
-        return done(user, created);
-      });
-    }
-  ));
-
-  app.get('/auth/google', passport.authenticate('google', { scope: [
-       'https://www.googleapis.com/auth/plus.login',
-       'https://www.googleapis.com/auth/plus.profile.emails.read'] 
-  }));
-
-  app.get( '/googlecallback', 
-      passport.authenticate( 'google', { 
-        successRedirect: 'http://localhost:3000/eventDetails',
-        // failureRedirect: '/googleLogin'
-  }));
-
-
-
-
-//body parse json
+//PARSING
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(bodyParser.json());
 
-//server up all static files
-app.use(express.static(path.join(__dirname + '/../client')));
+//SERVE UP STATIC FILES
+app.use(express.static(path.join(__dirname + '/../client/src')));
 
-app.get('/', function(req, res, next){
-  res.send(404);
-  // res.render('index.html');
-})
+//GOOGLE AUTHENTICATION
+require('./routes/config.js')(app, express);
+//TODO: further modularize code for routing use below line: 
+// require('./routes/PartyRouter.js')(app, express);
 
-//add routing
+//ROUTING
 var partyRouter = require(path.join(__dirname + '/routes/PartyRouter.js'));
-//add in optional url and additional routes if neccessary
-
 app.use(partyRouter);
 
-//app is listening on port 3000
+//START APP ON PORT 3000
 app.listen( port, function(err){
   if(err) {
     return console.log('error listening on port 3000', err);
