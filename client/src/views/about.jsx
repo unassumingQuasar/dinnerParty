96838c4657ba833a55118e3587bb32fdfe01ca46
Mark Tyneway <tyneway12@gmail.com>
2016-04-12 13:45:20 -0700

    Clean up code to follow Airbnb style guide

---
+++ client/src/views/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function about() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>We bring people together to socialize.</p>
+    </div>
+  );
+}
+
+export default about;
--- client/src/views/dash.jsx
+++ client/src/views/dash.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import CreatePartyForm from '../dashComponents/CreatePartyForm.jsx';
 import PartyDetailsTable from '../dashComponents/PartyDetailsTable.jsx';
 import GuestsTable from '../dashComponents/GuestListTable.jsx';
@@ -13,7 +14,7 @@ function dash() {
       <h3>Guest Details</h3>
       <GuestsTable />
     </div>
-  )
+  );
 }
 
 export default dash;
