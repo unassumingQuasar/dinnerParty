c8e582ea212b34ccbdda20870d3e371b0a812c0f
Mark Tyneway <tyneway12@gmail.com>
2016-04-12 13:44:48 -0700

    Import bootstrap css and less

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Redirect, Link, hashHistory } from 'react-router';
+import { Router, Route, Redirect, hashHistory } from 'react-router';
 
 import dash from './views/dash.jsx';
 import login from './views/login.jsx';
+import about from './views/about.jsx';
 
-const About = () => <div><h1>About</h1></div>;
+import 'bootstrap/less/bootstrap.less';
+import 'bootstrap/dist/css/bootstrap.css';
+// import '../node_modules/bootstrap/less/bootstrap.less';
+// import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 const App = () => {
   return (
     <Router history={ hashHistory }>
-      <Route path="/dash" component={dash}></Route>
-      <Route path="/about" component={About}></Route>
-      <Route path="/login" component={login}></Route>
-      <Redirect from="/" to="/login"></Redirect>
+      <Route path="/dash" component={dash} />
+      <Route path="/about" component={about} />
+      <Route path="/login" component={login} />
+      <Redirect from="/" to="/login" />
     </Router>
-  )
+  );
 };
 
-ReactDOM.render( <App />, document.getElementById('app') );
+ReactDOM.render(<App />, document.getElementById('app'));
