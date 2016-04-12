a13d167b884d2006b8f978a6995ceb5a45267fe3
Mark Tyneway <tyneway12@gmail.com>
2016-04-12 11:54:24 -0700

    Add css dependecies

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,30 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   entry: './src/app.jsx',
   output: {
-      path: path.join(__dirname),
-      publicPath: '/',
-      filename: 'bundle.js'
+    path: path.join(__dirname),
+    publicPath: '/',
+    filename: 'bundle.js'
   },
-  // devtool: 'source-map',
   module: {
     loaders: [
       {
-        test: /\.jsx?$/, //reg-ex for js & jsx
+        test: /\.jsx?$/, // reg-ex for js & jsx
         include: path.join(__dirname, 'src'),
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          //added react-require to fix React is not defined issue
-          //did not work
+          // added react-require to fix React is not defined issue
+          // react-require must go before 'transform-runtime'
           plugins: ['react-require', 'transform-runtime'],
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
+          presets: ['es2015', 'react'],
+        },
+      }, {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+      },
+    ],
   },
-  debug: true
+  debug: true,
 };
