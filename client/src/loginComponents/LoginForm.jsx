465fd8d4cccfb98d4c8332da54dee3d2119149af
CodeLizards <ebharris93@gmail.com>
2016-04-13 22:40:42 -0700

    added link so that login button reroutes to googleAuth page

--- client/src/loginComponents/LoginForm.jsx
+++ client/src/loginComponents/LoginForm.jsx
@@ -5,15 +5,14 @@ import TextField from 'material-ui/lib/text-field';
 class LoginForm extends React.Component {
   constructor() {
     super();
+  }
 }
-
 
 
   render() {
     return (
       <div>
         <button><a href="http://localhost:3000/auth/google">Login with Google</a></button>
-        // <button onClick={(event) => this.handleSubmit(event)} value='Login with google'>Login With Google</button>
       </div>
     );
   }
