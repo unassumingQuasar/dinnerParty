3da59f18d5729ea4510290e4f7931952e1f7ba08
CodeLizards <ebharris93@gmail.com>
2016-04-14 16:07:56 -0700

    implement getAllEvents gets all user events with guestlist for each event

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -1,5 +1,6 @@
 var Sequelize = require('sequelize');
 var db = require('../config/init.js');
+var each = require('async-each');
 
 //make a new event by a specific host
 exports.createEvent = function(req, res, next){
@@ -11,7 +12,7 @@ exports.createEvent = function(req, res, next){
           user.addEvent(event)
             .then(function(user){
               //send back  the event we just created so that it can be displayed in the user's event feed
-              res.send(event);
+              res.json(event.dataValues);
             })
         })
     });
@@ -35,7 +36,7 @@ exports.updateEvent = function(req, res, next){
                 event.updateAttributes(req.body)
                   .then(function(event){
                     //send back updated event so that it can be updated in user's event feed.
-                    res.json(event);
+                    res.json(event.dataValues);
                   })     
     
               })
@@ -48,22 +49,37 @@ exports.updateEvent = function(req, res, next){
 
 
 //and get associated guestlist for each event and send back on each event object
+// we want to send back [{name: , date: , etc: , guestlist: [ {user}, {user} , {user}]}, {}, ]
 exports.getAllEvents = function(req, res, next){
 
   var allEvents = [];
   db.User.findOne({googleId: req.user.googleId})
     .then(function(user){   
       user.getEvents().then(function(events){
-        console.log('EVENTS', events);
-        for(var i = 0; i < events.length; i++) {
-          allEvents[i] = events[i];
-          allEvents[i].guestlist = [];
-          AllGuests(req, events[i].name, function(users){
-            allEvents[i].guestlist = users;
-            next();
-          })
-        }
-        res.json(allEvents);
+
+
+        each(events, function(event, next){
+          
+          if(event.dataValues){
+            
+            event.getUsers().then(function(users){
+              var data = event.dataValues;
+              currentEvent = {id: data.id, eventName: data.name, description: data.description, location: data.location, date: data.date, cost: data.cost, guestlist: []};
+              for(var i = 0; i < users.length; i++){
+                if(users[i].dataValues){
+                  currentEvent.guestlist.push({name: users[i].dataValues.name, id: users[i].dataValues.id});      
+                }
+              }
+
+              allEvents.push(currentEvent);
+              next();
+            });
+
+          }
+        }, function(){
+          res.json(allEvents);
+        });
+
       });
     });
 
@@ -72,69 +88,49 @@ exports.getAllEvents = function(req, res, next){
 
 
 
-exports.getAllGuests = function(req, res){
+// exports.getAllGuests = function(req, res){
 
-// get all guests for specific event
-//get all users events first then find in the db 
-//the event that matches the specific event searched for
-//then get the users on that event
-// to allow users to be able to make events the same name as other users
+// // get all guests for specific event
+// //get all users events first then find in the db 
+// //the event that matches the specific event searched for
+// //then get the users on that event
+// // to allow users to be able to make events the same name as other users
 
-  db.User.findOne({googleId: req.user.googleId})
-    .then(function(user){
-      if(!event){
-        return console.log('there is no user logged in!')
-      }
-      user.getEvents().then(function(events){
-        for(var i = 0; i < events.length; i++){
-          if(events[i].name === req.eventName){
-            db.Event.findOne({id: events[i].id})
-              .then(function(event){
-                event.getUsers().then(function(users){
-                  res.json(users);
-                })
-              })
-          }
-        }
-      })
-  });
+//   db.User.findOne({googleId: req.user.googleId})
+//     .then(function(user){
+//       if(!user){
+//         return console.log('there is no user logged in!')
+//       }
+//       user.getEvents().then(function(events){
+//         for(var i = 0; i < events.length; i++){
+//           if(events[i].name === req.eventName){
+//             db.Event.findOne({id: events[i].id})
+//               .then(function(event){
+//                 event.getUsers().then(function(users){
+//                   res.json(users);
+//                 })
+//               })
+//           }
+//         }
+//       })
+//   });
 
-};
+// };
 
 
 
-exports.addGuest = function(req, res, next){
+exports.addGuest = function(req, res){
  //refractor to make sure that we are getting the users event
  // not some other user's event with the same name like (21st bday);
  db.User.findOne({name: req.body.guestName}).then(function(guest){
    db.Event.findOne({name: req.body.event})
    .then(function(event){
-     console.log('EVENT', event);
-     event.addUser(guest);
-     next();
+     event.addUser(guest, function(){
+       res.json(guest.dataValues);
+     });
    });
  });
 };
 
 
-var AllGuests = function(req, eventName, cb) {
-  db.User.findOne({googleId: req.user.googleId})
-    .then(function(user){
-      if(!event){
-        return console.log('there is no user logged in!')
-      }
-      user.getEvents().then(function(events){
-        for(var i = 0; i < events.length; i++){
-          if(events[i].name === eventName){
-            db.Event.findOne({id: events[i].id})
-              .then(function(event){
-                event.getUsers().then(function(users){
-                  cb(users);
-                })
-              })
-          }
-        }
-      })
-  });
-}
 
