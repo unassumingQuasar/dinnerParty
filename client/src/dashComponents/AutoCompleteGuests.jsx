5608ab96f8b4eb4f1806581e1ec42cba291c8794
Mark Tyneway <tyneway12@gmail.com>
2016-04-14 18:27:08 -0700

    Set up autocomplete module

---
+++ client/src/dashComponents/AutoCompleteGuests.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import AutoComplete from 'material-ui/lib/auto-complete';
+import get from '../utils/get.js';
+
+class AutoCompleteGuests extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: [],
+    };
+  }
+
+  // function that gets the dataSource
+  // GET /usernames
+  componentWillMount() {
+    this.buildDataSource('/usernames');
+  }
+  // componentWillMount is invoked once before the
+  // first render, see:
+  // http://javascript.tutorialhorizon.com/2014/09/13/execution-sequence-of-a-react-components-lifecycle-methods/
+  buildDataSource(url) {
+    get(url, (data) => {
+      this.setState({
+        dataSource: data,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <AutoComplete
+        hintText="Add friends!"
+        dataSource={this.state.dataSource}
+      />
+    );
+  }
+}
+
+export default AutoCompleteGuests;
