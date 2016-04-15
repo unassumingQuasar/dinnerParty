88ae942516a198ec746d42f343d992262e24aa91
Mark Tyneway <tyneway12@gmail.com>
2016-04-14 17:06:42 -0700

    Add date field to state

--- client/src/dashComponents/CreatePartyForm.jsx
+++ client/src/dashComponents/CreatePartyForm.jsx
@@ -3,7 +3,7 @@ import ajaxPost from '../utils/ajaxPost.jsx';
 // import get from '../utils/get.js';
 import InviteFriendForm from './InviteFriendForm.jsx';
 import PhotoUpload from './formComponents/PhotoUpload.jsx';
-
+import DatePicker from 'material-ui/lib/date-picker/date-picker';
 import TextField from 'material-ui/lib/text-field';
 import RaisedButton from 'material-ui/lib/raised-button';
 
@@ -15,6 +15,7 @@ class CreatePartyForm extends React.Component {
       location: '',
       time: '',
       cost: '',
+      date: '',
       description: '',
       inviteList: [],
       url: 'http://localhost:3000/event',
@@ -70,6 +71,12 @@ class CreatePartyForm extends React.Component {
             type="text"
             floatingLabelText= 'Location'
             onChange={this.onChange.bind(this, 'location')}
+          />
+          <TextField
+            id="date"
+            type="text"
+            floatingLabelText="Date"
+            onChange={this.onChange.bind(this, 'text')}
           />
           <TextField
             id="time"
