fca640b303be2607230a04ef88de1993f0756c02
Mark Tyneway <tyneway12@gmail.com>
2016-04-14 17:05:03 -0700

    Clean up extra comments

--- client/src/views/dash.jsx
+++ client/src/views/dash.jsx
@@ -3,8 +3,6 @@ import CreatePartyForm from '../dashComponents/CreatePartyForm.jsx';
 import PartyDetailsTable from '../dashComponents/PartyDetailsTable.jsx';
 import GuestsTable from '../dashComponents/GuestListTable.jsx';
 
-// http request here, pass down the data structure as props?
-
 function dash() {
   return (
     <div>
